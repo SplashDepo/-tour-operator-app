c8ee27d23ad26250b92843f7e33f25c9418fb865
d.8oulevard <d.8oulevard@yandex.ru>
2023-12-30 20:45:54 +0300

    add burger menu

--- src/index.js
+++ src/index.js
@@ -8,8 +8,21 @@ import { selectors } from "./utils/constants"
 
 const selectsContainer = document.querySelectorAll('.custom-select')
 
+const burgerMenu = document.querySelector('.menu')
+const burgerButton = document.querySelector('.burger-button')
+const burgerButtonLine = document.querySelector('.burger-button__line')
+
+function toggleBurgerVisible() {
+  burgerButton.classList.toggle('burger-button_active')
+  burgerButtonLine.classList.toggle('burger-button__line_active')
+  burgerMenu.classList.toggle('menu_active')
+}
+
+
 selectsContainer.forEach(select => {
   const selectElement = new CustomAccessibleSelect(select, keyCodes, selectors)
   selectElement.renderItems()
   selectElement.initEvents()
 })
+
+burgerButton.addEventListener('click', toggleBurgerVisible)
